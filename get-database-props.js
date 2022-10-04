57f4ce37dc4ff2ebe44042ec78c0a498364b87c8
Alex Zhang <alexzhang1618@gmail.com>
2022-10-03 20:04:37 -0700

    Improve readability of get-database-props.js

--- get-database-props.js
+++ get-database-props.js
@@ -1,4 +1,4 @@
-let dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -6,7 +6,8 @@ dotenv.config();
     console.log('Saving properties of prod calendar in file...');
     const notion = require('@notionhq/client');
     const client = new notion.Client({ auth: process.env.NOTION_INTEGRATION_TOKEN });
-    const database_props = await (await client.databases.retrieve({ database_id: process.env.NOTION_CALENDAR_ID })).properties;
+    const database_info = await client.databases.retrieve({ database_id: process.env.NOTION_CALENDAR_ID })
+    const database_props = await database_info.properties;
     console.log('Done querying! Saving to file...');
     require('fs').writeFileSync('database.properties', JSON.stringify(database_props, 2));
     console.log('Done!');
